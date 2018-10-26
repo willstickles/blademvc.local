c6bc1b0dd666dd5159ecae9174cf9c5986649e99
Will Stickles <will.stickles.sensei@gmail.com>
2018-10-26 13:48:56 -0400

    MVC Framework that I wrote for practice. It has basic account creation with email activation tokens, CSRF tokens, etc.

---
+++ blademvc.local/App/Controllers/Profile.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Flash;
+use \Core\View;
+use \App\Auth;
+
+/**
+ * Profile controller
+ *
+ * PHP version 7.0.2-22
+ */
+class Profile extends Authenticated
+{
+	/**
+	 * Before filter - called before each action method
+	 *
+	 * @return void
+	 */
+	protected function before() {
+		parent::before();
+		
+		$this->user = Auth::getUser();
+	}
+	/**
+	 * Show the profile
+	 *
+	 * @return void
+	 */
+	public function showAction() {
+		View::renderTemplate( 'Profile.show', [
+			'user'  =>  $this->user
+		] );
+	}
+	
+	/**
+	 * Show the form for editing the profile
+	 *
+	 * @return void
+	 */
+	public function editAction() {
+		View::renderTemplate( 'Profile.edit', [
+			'user'  =>  $this->user
+		] );
+	}
+	
+	/**
+	 * Update the profile
+	 *
+	 * @return void
+	 */
+	public function updateAction() {
+
+		
+		if ( $this->user->updateProfile( $_POST ) ) {
+			
+			Flash::addMessage( 'Changes saved' );
+			
+			$this->redirect( '/profile/show' );
+			
+		} else {
+			
+			View::renderTemplate( 'Profile.edit', [
+				'user'  =>  $this->user
+			]);
+			
+		}
+	}
+}
